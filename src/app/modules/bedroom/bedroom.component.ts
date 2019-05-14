cb778678c8379c2ae934a44ba6fa3d7b974634a3
vicky <vgonsalves@arcfix.com>
2019-05-14 15:19:03 +0530

    Added support for local server connection for bedroom

--- src/app/modules/bedroom/bedroom.component.ts
+++ src/app/modules/bedroom/bedroom.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {fromEvent, merge, Observable, of} from 'rxjs';
-import {mapTo} from 'rxjs/operators';
+import {DomSanitizer} from '@angular/platform-browser';
+import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 import {Bedroom} from '../../shared/models/bedroom';
 import {BedroomService} from '../../shared/services/bedroom/bedroom.service';
 
@@ -13,16 +14,10 @@ export class BedroomComponent implements OnInit {
   title = 'Home Automation';
   bedroomStatus: Bedroom;
   online$: Observable<boolean>;
+  frameUrl: any;
 
-  constructor(private bedroomService: BedroomService) {
-    this.online$ = merge(
-      of(navigator.onLine),
-      fromEvent(window, 'online').pipe(mapTo(true)),
-      fromEvent(window, 'offline').pipe(mapTo(false))
-    );
-    this.online$.subscribe(ree => {
-      console.log(ree);
-    });
+  constructor(private bedroomService: BedroomService, private sanitizer: DomSanitizer) {
+    this.frameUrl = this.sanitizer.bypassSecurityTrustResourceUrl(environment.localBedroomHost);
   }
 
   ngOnInit() {
@@ -38,29 +33,11 @@ export class BedroomComponent implements OnInit {
   }
 
   changeLightMode(light: number, mode: boolean) {
-    const online = this.bedroomStatus.websocket === 'connected';
-    let action = '';
-    if (online && this.online$) {
-      if (light === 2) {
-        this.bedroomService.putBedroomStatus({light2: mode, updatedByDevice: false});
-      }
-      if (light === 4) {
-        this.bedroomService.putBedroomStatus({fan: mode, updatedByDevice: false});
-      }
-    } else {
-      if (light === 2 && mode === true) {
-        action = 'light2on';
-      } else if (light === 2 && mode === false) {
-        action = 'light2off';
-      } else if (light === 4 && mode === true) {
-        action = 'fanon';
-      } else if (light === 4 && mode === false) {
-        action = 'fanoff';
-      }
-      this.bedroomService.performLocalAction(action).subscribe(response => {
-        console.log(response);
-        this.bedroomStatus = response;
-      });
+    if (light === 2) {
+      this.bedroomService.putBedroomStatus({light2: mode, updatedByDevice: false});
+    }
+    if (light === 4) {
+      this.bedroomService.putBedroomStatus({fan: mode, updatedByDevice: false});
     }
   }
 }
--- src/app/shared/services/bedroom/bedroom.service.ts
+++ src/app/shared/services/bedroom/bedroom.service.ts
@@ -1,27 +1,18 @@
-import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Socket} from 'ngx-socket-io';
-import {Observable} from 'rxjs';
-import {environment} from '../../../../environments/environment';
 import {Bedroom} from '../../models/bedroom';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BedroomService {
-  private apiUrl: string = environment.localBedroomHost;
   bedroomStatus = this.socket.fromEvent<Bedroom>('bedroom:init');
   updatedBedroomStatus = this.socket.fromEvent<Bedroom>('bedroom:save');
 
-  constructor(private socket: Socket,
-              private http: HttpClient) {
+  constructor(private socket: Socket) {
   }
 
   putBedroomStatus(data: any) {
     this.socket.emit('bedroom:put', data);
-  }
-
-  public performLocalAction(action: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}${action}`);
   }
 }
