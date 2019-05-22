4b4a127720e2217bf1ebdf3622b3d3c4b20ff951
vicky <vgonsalves@arcfix.com>
2019-05-22 21:59:46 +0530

    Changed config for devlopment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  domain: 'https://tanksystem.herokuapp.com',
-  apiUrl: 'https://tanksystem.herokuapp.com/api/',
+  domain: 'https://tanksystem-dev.herokuapp.com',
+  apiUrl: 'https://tanksystem-dev.herokuapp.com/api/',
   localBedroomHost: 'http://192.168.3.9/'
 };
